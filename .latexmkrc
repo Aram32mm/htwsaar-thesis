3e325addfa2a1e748c83557eb12e8757e72a5a21
José Aram Méndez Gómez <104022682+Aram32mm@users.noreply.github.com>
2025-05-25 09:04:57 +0200

    latexmk script set up for build

---
+++ .latexmkrc
@@ -0,0 +1,5 @@
+$pdf_mode = 1;
+$bibtex_use = 2;
+$pdflatex = 'pdflatex -interaction=nonstopmode -synctex=1 %O %S';
+$preview_continuous_mode = 1;
+$clean_ext = "aux bbl blg idx ind lof lot out toc acn acr alg glg glo gls ist nlg nlo nls xdy run.xml";
---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Build script for HTW Saar thesis template
+
+MAIN_FILE="htwsaar-i-mst-vorlage.tex"
+
+# Check for command line arguments
+if [ "$1" == "clean" ]; then
+    echo "Cleaning auxiliary files..."
+    latexmk -c "$MAIN_FILE"
+    exit 0
+elif [ "$1" == "once" ]; then
+    echo "Building PDF once..."
+    latexmk -pdf "$MAIN_FILE"
+    exit 0
+fi
+
+# Default: continuous build
+echo "Starting continuous build mode. Press Ctrl+C to exit."
+echo "Document will automatically rebuild when changes are detected."
+latexmk -pdf -pvc "$MAIN_FILE"
